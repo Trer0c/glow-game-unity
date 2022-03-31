d18ffa78ea95bb85662474ce6e9608bbc37d49a8
unknown <v.smirnov@aplink.ru>
2022-03-31 11:00:58 +0300

    1.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,20 +8,27 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Image[] _imageWalls;
     [SerializeField] private Sprite[] _spritePlayer;
+    [SerializeField] private Sprite[] _spriteWalls;
     [SerializeField] private Image _imagePlayer;
     [SerializeField] private Text _textScore;
     [SerializeField] private Text _textRecord;
+    [SerializeField] private GameObject _btnRestart;
     private int[] _indexColorWalls = new int[] { 1, 0 };
     private int _indexColorPlayer = 0;
     private int _score = 0;
     private int _record = 0;
     private float _speed = 4;
+    private bool start;
+    private bool up;
+    private bool bottom;
 
     Vector2 direction;
 
     private void Start()
     {
         direction = new Vector2(10, 0);
+        start = true;
+        _btnRestart.SetActive(false);
         _record = PlayerPrefs.GetInt("Record");
         _textScore.text = "Score: " + _score.ToString();
         _textRecord.text = "Record: " + _record.ToString();
@@ -39,6 +46,7 @@ public class Player : MonoBehaviour
             if (_indexColorWalls[0] != _indexColorPlayer)
             {
                 Time.timeScale = 0f;
+                _btnRestart.SetActive(true);
                 return;
             }
             int random = Random.Range(0, 2);
@@ -46,14 +54,39 @@ public class Player : MonoBehaviour
             _imagePlayer.sprite = _indexColorPlayer == 0 ? _spritePlayer[0] : _spritePlayer[1];
             direction = new Vector2(10f, 0);
             _score += 1;
-            _speed += (_speed / 100) * 1;
-            print(_speed);
             _textScore.text = "Score: " + _score.ToString();
             if (_score >= _record)
             {
                 _record = _score;
                 PlayerPrefs.SetInt("Record", _record);
                 _textRecord.text = "Record: " + _record.ToString();
+            }
+            if (start)
+            {
+                _speed += 0.04f;
+                if (_speed >= 5)
+                {
+                    start = false;
+                    up = true;
+                }
+            }
+            else if (up)
+            {
+                _speed += 0.04f;
+                if (_speed >= 6)
+                {
+                    up = false;
+                    bottom = true;
+                }
+            }
+            else if (bottom)
+            {
+                _speed -= 0.04f;
+                if (_speed <= 5)
+                {
+                    up = true;
+                    bottom = false;
+                }
             }
         }
         else if (other.transform.name == "WallRight")
@@ -61,6 +94,7 @@ public class Player : MonoBehaviour
             if (_indexColorWalls[1] != _indexColorPlayer)
             {
                 Time.timeScale = 0f;
+                _btnRestart.SetActive(true);
                 return;
             }
             int random = Random.Range(0, 2);
@@ -68,8 +102,6 @@ public class Player : MonoBehaviour
             _imagePlayer.sprite = _indexColorPlayer == 0 ? _spritePlayer[0] : _spritePlayer[1];
             direction = new Vector2(-10f, 0);
             _score += 1;
-            _speed += (_speed / 100) * 1;
-            print(_speed);
             _textScore.text = "Score: " + _score.ToString();
             if (_score >= _record)
             {
@@ -77,7 +109,35 @@ public class Player : MonoBehaviour
                 PlayerPrefs.SetInt("Record", _record);
                 _textRecord.text = "Record: " + _record.ToString();
             }
+            if (start)
+            {
+                _speed += 0.04f;
+                if (_speed >= 5)
+                {
+                    start = false;
+                    up = true;
+                }
+            }
+            else if (up)
+            {
+                _speed += 0.04f;
+                if (_speed >= 6)
+                {
+                    up = false;
+                    bottom = true;
+                }
+            }
+            else if (bottom)
+            {
+                _speed -= 0.04f;
+                if (_speed <= 5)
+                {
+                    up = true;
+                    bottom = false;
+                }
+            }
         }
+        print(_speed);
     }
 
     public void SwitchColorWalls()
@@ -87,9 +147,9 @@ public class Player : MonoBehaviour
         for (int i = 0; i < 2; i++)
         {
             if (_indexColorWalls[i] == 0)
-                _imageWalls[i].color = new Color(255, 255, 255, 255);
+                _imageWalls[i].sprite = _spriteWalls[0];
             else if (_indexColorWalls[i] == 1)
-                _imageWalls[i].color = new Color(0, 0, 0, 255);
+                _imageWalls[i].sprite = _spriteWalls[1];
         }
     }
 
